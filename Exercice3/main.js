31b05b61149ab60b652e70a2433e3ab18c83675d
orewan <42575815+orewan@users.noreply.github.com>
2023-06-12 19:27:07 +0300

    Add files via upload

---
+++ Exercice3/main.js
@@ -0,0 +1,48 @@
+//console.log("Hi ! How are you  ? ");
+
+const a = 5;
+const b =10;
+
+let x;
+const c=6;
+x=a+b;
+
+console.log(x);
+console.log(x+c);
+
+
+
+
+// npm install -g nodemon
+
+
+/*
+To see a list of supported npm commands, run:
+  npm help
+PS D:\DevInstitue-Miniproject\Miniproject\Di-bootCamp-Week11\Exercise3> npm install -g nodemon
+
+added 32 packages in 2s
+
+3 packages are looking for funding
+  run `npm fund` for details
+PS D:\DevInstitue-Miniproject\Miniproject\Di-bootCamp-Week11\Exercise3> 
+*/
+
+
+
+/*
+nodemon] restarting due to changes...
+[nodemon] starting `node main.js`
+[nodemon] restarting due to changes...
+15
+21
+[nodemon] starting `node main.js`
+15
+21
+[nodemon] clean exit - waiting for changes before restart
+[nodemon] restarting due to changes...
+[nodemon] starting `node main.js`
+15
+21
+[nodemon] clean exit - waiting for changes before restart
+*/
